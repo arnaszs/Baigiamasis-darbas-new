ea80c6b5f7cce279f451565d6e6d600ff210abe0
arnaszs <bolisas8@gmail.com>
2023-07-12 13:54:44 +0300

    media, settings, urls, error with showing pictures on products

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -111,6 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
+LANGUAGES = (
+    (LANGUAGE_CODE, 'English'),
+    ('lt', 'Lietuvių'),
+)
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
@@ -121,11 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath(STATIC_URL)
+MEDIA_URL = "/images/"
+MEDIA_ROOT = BASE_DIR.joinpath(MEDIA_URL,'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
